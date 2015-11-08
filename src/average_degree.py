bf6ac70a2f9840e812ac1869bdb8bb5696c44eea
Michael Raimi <mar2260@columbia.edu>
2015-11-08 17:00:00 -0500

    Create graph and calculate average

--- src/average_degree.py
+++ src/average_degree.py
@@ -3,13 +3,58 @@ import json, sys
 source = open(sys.argv[1], 'r')
 dest = open(sys.argv[2], 'w')
 
-for line in source:
-    jl = json.loads(line)
-    if 'entities' in jl:
-        if 'hashtags' in jl['entities']:
-            for ht in jl['entities']['hashtags']:
-                if 'text' in ht:
-                    dest.write(ht['text'].encode('ascii','ignore')+' ');
-            dest.write('\n')
-source.close()
-dest.close()
+def main():
+    edges = 0
+    vertices = 0
+    graph = dict()
+    for line in source:
+        jl = json.loads(line)
+        hashtags = []
+        if 'entities' in jl:
+            if 'hashtags' in jl['entities']:
+                for ht in jl['entities']['hashtags']:
+                    if 'text' in ht:
+                        hashtags.append(ht['text'].encode('ascii','ignore').lower());
+        print str(hashtags)
+        newVals = pairify(hashtags, graph, edges, vertices)
+        edges = newVals[0]
+        vertices = newVals[1]
+        dest.write("Avg: " + str(getAverage(edges, vertices)) + "\n")
+    source.close()
+    dest.close()
+
+def pairify(hashtags, graph, edges, vertices):
+    for first in xrange(0,len(hashtags)-1):
+        for second in xrange(first+1,len(hashtags)):
+            if hashtags[first] not in graph:
+                vertices += 1
+                edges += 1
+                if hashtags[second] not in graph:
+                    vertices += 1
+                    graph[hashtags[first]] = [hashtags[second]]
+                    graph[hashtags[second]] = [hashtags[first]]
+                else:
+                    graph[hashtags[first]] = [hashtags[second]]
+                    graph[hashtags[second]].append(hashtags[first])
+            elif hashtags[first] in graph:
+                if hashtags[second] not in graph:
+                    vertices += 1
+                    edges += 1
+                    graph[hashtags[first]].append(hashtags[second])
+                    graph[hashtags[second]] = [hashtags[first]]
+                elif hashtags[second] not in graph[hashtags[first]]:
+                    graph[hashtags[first]].append(hashtags[second])
+                    graph[hashtags[second]].append(hashtags[first])
+    return [edges,vertices]
+
+def getAverage(edges, vertices):
+    return 2 * (float(edges) / float(vertices)) if (vertices > 0) else 0
+
+if __name__ == "__main__":
+    main()
+
+
+# to do:
+# sanitize hashtags, ignore
+# pairify
+# calculate average
--- src/tweets_cleaned.py
+++ src/tweets_cleaned.py
@@ -1,26 +1,31 @@
 import json, sys
-source = open(sys.argv[1], 'r')
-dest = open(sys.argv[2], 'w')
-unicodeCount = 0
-for line in source:
-    text = ''
-    created = ''
-    jl = json.loads(line)
-    if 'text' in jl:
-        s = jl['text']
-        text = s.encode('ascii','ignore')
-        text = text.replace('\n',' ').replace('\t',' ')
 
-        # Determines whether a tweet has unicode by assuming it's ascii.
-        # If an encode error is thrown that means the string contained
-        # some unicode at which point we bump the unicode counter.
-        try:
-            s.decode('ascii')
-        except UnicodeEncodeError:
-            unicodeCount += 1
-    if 'created_at' in jl:
-        created = jl['created_at']
-    dest.write(text + ' (timestamp: ' + created + ')\n')
-dest.write('\n'+ str(unicodeCount) + ' tweet(s) contained unicode.\n')
-source.close()
-dest.close()
+def main():
+    source = open(sys.argv[1], 'r')
+    dest = open(sys.argv[2], 'w')
+    unicodeCount = 0
+    for line in source:
+        text = ''
+        created = ''
+        jl = json.loads(line)
+        if 'text' in jl:
+            s = jl['text']
+            text = s.encode('ascii','ignore')
+            text = text.replace('\n',' ').replace('\t',' ')
+
+            # Determines whether a tweet has unicode by assuming it's ascii.
+            # If an encode error is thrown that means the string contained
+            # some unicode at which point we bump the unicode counter.
+            try:
+                s.decode('ascii')
+            except UnicodeEncodeError:
+                unicodeCount += 1
+        if 'created_at' in jl:
+            created = jl['created_at']
+        dest.write(text + ' (timestamp: ' + created + ')\n')
+    dest.write('\n'+ str(unicodeCount) + ' tweet(s) contained unicode.\n')
+    source.close()
+    dest.close()
+
+if __name__ == "__main__":
+    main()
