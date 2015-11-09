4cef066c24a23f4a2ed4c9d3b9b1a74cc06fe535
Michael Raimi <mar2260@columbia.edu>
2015-11-08 20:45:42 -0500

    Maintain 60 sec window

--- src/average_degree.py
+++ src/average_degree.py
@@ -7,16 +7,23 @@ def main():
     edges = 0
     vertices = 0
     graph = dict()
+    ordered = []
+    created = None
     for line in source:
         jl = json.loads(line)
+        if 'limit' in jl:
+            continue
         hashtags = []
         if 'entities' in jl:
             if 'hashtags' in jl['entities']:
                 for ht in jl['entities']['hashtags']:
                     if 'text' in ht:
                         hashtags.append(ht['text'].encode('ascii','ignore').lower());
-        print str(hashtags)
-        newVals = pairify(hashtags, graph, edges, vertices)
+        created = jl['created_at']
+        if 'created_at' in jl:
+            ordered.append([created] + hashtags)
+        postPruneVals = prune(jl['created_at'],edges,vertices,ordered,graph)
+        newVals = pairify(hashtags, graph, postPruneVals[0], postPruneVals[1])
         edges = newVals[0]
         vertices = newVals[1]
         dest.write("Avg: " + str(getAverage(edges, vertices)) + "\n")
@@ -27,13 +34,14 @@ def pairify(hashtags, graph, edges, vertices):
     for first in xrange(0,len(hashtags)-1):
         for second in xrange(first+1,len(hashtags)):
             if hashtags[first] not in graph:
-                vertices += 1
                 edges += 1
                 if hashtags[second] not in graph:
-                    vertices += 1
-                    graph[hashtags[first]] = [hashtags[second]]
-                    graph[hashtags[second]] = [hashtags[first]]
+                    if hashtags[first] != hashtags[second]:
+                        vertices += 2
+                        graph[hashtags[first]] = [hashtags[second]]
+                        graph[hashtags[second]] = [hashtags[first]]
                 else:
+                    vertices += 1
                     graph[hashtags[first]] = [hashtags[second]]
                     graph[hashtags[second]].append(hashtags[first])
             elif hashtags[first] in graph:
@@ -43,12 +51,46 @@ def pairify(hashtags, graph, edges, vertices):
                     graph[hashtags[first]].append(hashtags[second])
                     graph[hashtags[second]] = [hashtags[first]]
                 elif hashtags[second] not in graph[hashtags[first]]:
-                    graph[hashtags[first]].append(hashtags[second])
-                    graph[hashtags[second]].append(hashtags[first])
+                    if hashtags[first] != hashtags[second]: 
+                        edges += 1
+                        graph[hashtags[first]].append(hashtags[second])
+                        graph[hashtags[second]].append(hashtags[first])
     return [edges,vertices]
 
 def getAverage(edges, vertices):
-    return 2 * (float(edges) / float(vertices)) if (vertices > 0) else 0
+    return round(2 * (float(edges) / float(vertices)),2) if (vertices > 0) else 0.00
+
+def getSeconds(timestamp):
+    ts = timestamp.split(' ')
+    nums = ts[3].split(':')
+    return (int(nums[0])*60*60) + (int(nums[1])*60) + (int(nums[2]))
+
+def prune(timestamp, edges, vertices, ordered, graph):
+    cpy = ordered[:]
+    for record in cpy:
+        if getSeconds(timestamp) - getSeconds(record[0]) > 60:
+            newVals = _prune(graph, record[1:], edges, vertices)
+            edges = newVals[0]
+            vertices = newVals[1]
+            ordered.remove(record)
+        else:
+            return [edges, vertices]
+
+    return [edges,vertices]
+
+def _prune(graph, hashtags, edges, vertices):
+    for first in xrange(0,len(hashtags)-1):
+        for second in xrange(first+1,len(hashtags)):
+            graph[hashtags[first]].remove(hashtags[second])
+            graph[hashtags[second]].remove(hashtags[first])
+            edges -= 1
+            if len(graph[hashtags[first]]) == 0:
+                graph.remove(hashtags[first])
+                vertices -= 1
+            if len(graph[hashtags[second]]) == 0:
+                graph.remove(hashtags[second])
+                vertices -= 1
+    return [edges,vertices]
 
 if __name__ == "__main__":
     main()
